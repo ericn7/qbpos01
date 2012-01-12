088b4afa1d2bb6ab907c641bade6a6f97617681a
Eric Nelson <eric.mckenzie.nelson@gmail.com>
2012-01-11 22:37:57 -0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Qbpos01::Application.config.secret_token = '2fdf44d4768c0170e0eb7d9037004cc60a5b12c098622b1838829a39cb71c370df5c990335dd9bea92a88d700a5fcda345c8741a4d724cba7fba1bc34c6c6e29'
